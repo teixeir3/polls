c702bdf5ec9552d1d089b1faa4d7d87a71c877b0
Doug+Mickey <Doug+Mickey@appacademy.io>
2014-01-16 10:17:39 -0500

    Adds AnswerChoice associations.

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -11,4 +11,18 @@
 
 class AnswerChoice < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  belongs_to(
+    :question,
+    :class_name => "Question",
+    :foreign_key => :question_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :responses,
+    :class_name => "Response",
+    :foreign_key => :answer_choice_id,
+    :primary_key => :id
+  )
 end
