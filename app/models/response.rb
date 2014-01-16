a2c66c68258a52f3ec3e55f92f6b193c423bf177
Doug+Mickey <Doug+Mickey@appacademy.io>
2014-01-16 10:21:10 -0500

    Adds Response associations.

--- app/models/response.rb
+++ app/models/response.rb
@@ -12,4 +12,18 @@
 
 class Response < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  belongs_to(
+    :answer_choice,
+    :class_name => "AnswerChoice",
+    :foreign_key => :answer_choice_id,
+    :primary_key => :id
+  )
+
+  belongs_to(
+    :respondent,
+    :class_name => "User",
+    :foreign_key => :responder_id,
+    :primary_key => :id
+  )
 end
