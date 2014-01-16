541affd7fe3f07872023dcb198355eff06a02803
Doug+Mickey <Doug+Mickey@appacademy.io>
2014-01-16 10:04:49 -0500

    Adds Poll associations.

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -12,4 +12,17 @@
 class Poll < ActiveRecord::Base
   # attr_accessible :title, :body
 
+  belongs_to(
+    :author,
+    :class_name => 'User',
+    :foreign_key => :author_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :questions,
+    :class_name => 'Question',
+    :foreign_key => :poll_id,
+    :primary_key => :id
+  )
 end
