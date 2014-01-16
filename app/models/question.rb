9c8000405225f6dc16756706b7e9cdb80cfb1576
Doug+Mickey <Doug+Mickey@appacademy.io>
2014-01-16 10:14:14 -0500

    Adds Question associations.

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,4 +11,19 @@
 
 class Question < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  has_many(
+    :answer_choices,
+    :class_name => "AnswerChoice",
+    :foreign_key => :question_id,
+    :primary_key => :id
+  )
+
+  belongs_to(
+    :poll,
+    :class_name => "Poll",
+    :foreign_key => :poll_id,
+    :primary_key => :id
+  )
+
 end
